04a991b570b6ec3c5415161016260d26f45a155c
C1327624 Michael Medina da Cunha <c1327624@interno.bb.com.br>
2023-05-13 11:19:01 -0300

    chore: estruturas basicas do python

---
+++ basico/tipos_dados/string.py
@@ -0,0 +1,25 @@
+
+# String -> str -> tudo que esta entre aspas
+# "ISSO É UMA STRING"
+# "100" É UMA STRING
+
+
+# TIPO DE TIPAGEM = Dinâmica / Forte
+
+# Python infere o tipo de dado recebido em linguagens estáticas tu precisa colocar explicitamente o tipo
+# string nome = 'texto'
+# int idade = 26
+
+print(type(100))
+print(type("string"))
+print(type(10.55))
+
+# Tipagem forte
+# Não converte automaticamente um tipo
+# O Código abaixo resultaria no erro
+# TypeError: can only concatenate str (not "int") to str
+# print("Nome: Nao Sei, Idade: " + 10)
+
+
+
+# Aspas Simples
