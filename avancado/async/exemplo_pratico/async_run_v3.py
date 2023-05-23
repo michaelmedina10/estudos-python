2b203c4538e62881fba60f8b0f4d5c154504d862
michaelmedina10 <michael.cunha97@hotmail.com>
2023-05-23 07:33:59 -0300

    refactor: otimizando requests, transformando em funcoes

---
+++ avancado/async/exemplo_pratico/async_run_v3.py
@@ -0,0 +1,34 @@
+import asyncio
+import aiohttp
+import requests
+import os
+import time
+import warnings
+from dotenv import load_dotenv
+warnings.filterwarnings('ignore')
+load_dotenv()
+
+API_KEY = os.getenv('ALPHA_VANTAGE_KEY')
+url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}'
+symbols = ['AAPL', 'GOOG', 'TSLA', 'MSFT', 'AAPL', 'IBM', 'IBM', 'VALE', 'PETR', 'BBAS', 'ITUB', 'HAPV', 'MGLU', 'PRIO3', 'GGBR']
+results = []
+
+
+async def fazer_request(url):
+    session = requests.Session()
+    print(url)
+    session.verify = False
+    respose = await asyncio.get_event_loop().run_in_executor(None, session.get, url)
+    return respose.json()
+
+
+async def agregador_requisicao():
+    dados = await asyncio.gather(*[fazer_request(url.format(symbol, API_KEY)) for symbol in symbols])
+    return dados
+
+
+inicio = time.time()
+dados = asyncio.run(agregador_requisicao())
+fim = time.time() - inicio
+
+print(f"Tempo de Execução: {fim}")
