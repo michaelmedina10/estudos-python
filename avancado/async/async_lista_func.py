b23615328df959fb9848efd50d82135c2ed95bbc
C1327624 Michael Medina da Cunha <c1327624@interno.bb.com.br>
2023-06-27 16:01:19 -0300

    formas de otimizar o async

--- avancado/async/async_lista_func.py
+++ avancado/async/async_lista_func.py
@@ -1,42 +1,51 @@
 import pandas as pd
 import asyncio
 
-'''
-Entendi, no seu exemplo as funções A, B e C são chamadas dentro da função executar_consultas_assincronas
-através da função asyncio.gather. Nesse caso, a palavra-chave await não é necessária nas funções individuais
-porque o objeto retornado por asyncio.gather já espera os resultados de todas as corrotinas passadas como argumento.
-
-Ao usar asyncio.gather, você está criando uma tarefa que contém várias corrotinas, e essa tarefa
-será executada em paralelo utilizando o loop de eventos do asyncio. Assim, o loop de eventos
-irá esperar o término de todas as corrotinas incluídas na tarefa antes de continuar com a execução do programa.
-
-Dessa forma, quando a função executar_consultas_assincronas chama asyncio.gather(consulta_banco_1(), consulta_banco_2(), consulta_banco_3()), ela
-cria uma tarefa que contém as três corrotinas e aguarda até que
-todas essas corrotinas sejam concluídas antes de retornar um valor para a variável df_list.
-Portanto, nesse caso, a palavra-chave await não é necessária nas funções individuais.
-'''
-
 async def consulta_banco_1():
     # código da consulta
-    return pd.DataFrame({'coluna1': [1, 2, 3], 'coluna2': ['a', 'b', 'c']})
+    df = pd.DataFrame({'coluna1': [1, 2, 3], 'coluna2': ['a', 'b', 'c']})
+    df['nome_funcao'] = 'func1'
+    return df
 
 async def consulta_banco_2():
     # código da consulta
-    return pd.DataFrame({'coluna3': [4, 5, 6], 'coluna4': ['d', 'e', 'f']})
+    df = pd.DataFrame({'coluna3': [4, 5, 6], 'coluna4': ['d', 'e', 'f']})
+    df['nome_funcao'] = 'func2'
+    return df
 
 async def consulta_banco_3():
     # código da consulta
-    return pd.DataFrame({'coluna5': [7, 8, 9], 'coluna6': ['g', 'h', 'i']})
+    df = pd.DataFrame({'coluna5': [7, 8, 9], 'coluna6': ['g', 'h', 'i']})
+    df['nome_funcao'] = 'func3'
+    return df
 
 async def executar_consultas_assincronas():
-    tasks = [consulta_banco_1(), consulta_banco_2(),consulta_banco_3()]
-    df_list = await asyncio.gather(*tasks)
-    return df_list
+    tasks = [
+        consulta_banco_1(),
+        consulta_banco_2(),
+        consulta_banco_3()
+    ]
+    df_list = await asyncio.gather(*[task for task in tasks])
+    resultado = {}
+    for df in df_list:
+        nome_funcao = df['nome_funcao'][0]
+        del df['nome_funcao']
+        resultado[nome_funcao] = df
+    return resultado
 
-if __name__ == '__main__':
-    resultado = asyncio.run(executar_consultas_assincronas())
-    print(len(resultado))
-    print(resultado[0])
-    print(resultado[1])
-    print(resultado[2])
-    print(type(resultado[0]))
+async def minha_funcao():
+    # outras operações aqui
+    resultados = await executar_consultas_assincronas()
+    return resultados
+    # processar os resultados aqui
+
+# Chamada da função minha_funcao()
+loop = asyncio.get_event_loop()
+resultados = loop.run_until_complete(minha_funcao())
+loop.close()
+
+print(type(resultados))
+print(resultados.keys())
+print(resultados['func1'])
+print(resultados['func2'])
+print(resultados['func3'])
