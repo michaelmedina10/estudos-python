36efa7cb98153d3a120de4b38537bb6a4716418a
C1327624 Michael Medina da Cunha <c1327624@interno.bb.com.br>
2023-06-28 07:27:50 -0300

    melhorando codigo assincrono para consultas em bancos de dados

--- avancado/async/async_lista_func.py
+++ avancado/async/async_lista_func.py
@@ -9,29 +9,20 @@ async def consulta_banco_1():
 
 async def consulta_banco_2():
     # código da consulta
-    df = pd.DataFrame({'coluna3': [4, 5, 6], 'coluna4': ['d', 'e', 'f']})
+    df = pd.DataFrame({'coluna1': [4, 5, 6], 'coluna2': ['d', 'e', 'f']})
     df['nome_funcao'] = 'func2'
     return df
 
 async def consulta_banco_3():
     # código da consulta
-    df = pd.DataFrame({'coluna5': [7, 8, 9], 'coluna6': ['g', 'h', 'i']})
+    df = pd.DataFrame({'coluna1': [7, 8, 9], 'coluna2': ['g', 'h', 'i']})
     df['nome_funcao'] = 'func3'
     return df
 
 async def executar_consultas_assincronas():
-    tasks = [
-        consulta_banco_1(),
-        consulta_banco_2(),
-        consulta_banco_3()
-    ]
+    tasks = [consulta_banco_1(), consulta_banco_2(), consulta_banco_3()]
     df_list = await asyncio.gather(*[task for task in tasks])
-    resultado = {}
-    for df in df_list:
-        nome_funcao = df['nome_funcao'][0]
-        del df['nome_funcao']
-        resultado[nome_funcao] = df
-    return resultado
+    return df_list
 
 async def minha_funcao():
     # outras operações aqui
@@ -40,12 +31,5 @@ async def minha_funcao():
     # processar os resultados aqui
 
 # Chamada da função minha_funcao()
-loop = asyncio.get_event_loop()
-resultados = loop.run_until_complete(minha_funcao())
-loop.close()
-
-print(type(resultados))
-print(resultados.keys())
-print(resultados['func1'])
-print(resultados['func2'])
-print(resultados['func3'])
+resultados = asyncio.run(minha_funcao())
+print(pd.concat([df for df in resultados], ignore_index=True))
