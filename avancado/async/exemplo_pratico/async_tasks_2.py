12d57e3d880e0f32f9a23c02c64b527cd26277cc
michaelmedina10 <michael.cunha97@hotmail.com>
2023-07-04 08:16:26 -0300

    mais estudos sobre assincronismo no python

---
+++ avancado/async/exemplo_pratico/async_tasks_2.py
@@ -0,0 +1,38 @@
+import asyncio
+import time
+
+
+async def coleta_banco_1():
+
+    await asyncio.sleep(2)
+    print("terminei de coletar do banco de dados!")
+    return True
+
+async def coleta_banco_2():
+
+    await asyncio.sleep(2)
+    print("Terminei de coletar do banco de dados!!")
+    return True
+
+async def coleta_endpoint():
+
+    await asyncio.sleep(2)
+    print("Terminei de coletar do banco de dados!!")
+    return {"data": [{"coletado": "sucesso"}]}
+
+async def main():
+    inicio = time.time()
+    task1 = asyncio.create_task(coleta_banco_1())
+    task2 = asyncio.create_task(coleta_banco_2())
+    task3 = asyncio.create_task(coleta_endpoint())
+
+    resutado1 = await task1
+    resutado2 = await task2
+    resutado3 = await task3
+
+    print(resutado1)
+    print(resutado2)
+    print(resutado3)
+    print(f"FIM: {time.time() - inicio}")
+
+asyncio.run(main())
