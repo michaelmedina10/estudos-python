bbf0c07f1551584a131f45ac98c6a766a8d42f54
michaelmedina10 <michael.cunha97@hotmail.com>
2023-08-17 15:57:41 -0300

    reactpy teste

---
+++ reactpy/src.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from reactpy import component, html
+from reactpy.backend.fastapi import configure
+
+@component
+def helloWorld():
+    return html.h1("Quem sou eu!")
+
+@component
+def helloWorld2():
+    return html.h2("Sou outra funcao e agr???")
+
+@component
+def PrintButton(display_text, message_text):
+    def handle_event(event):
+        print(message_text)
+
+    return html.button({"on_click": handle_event}, display_text)
+
+@component
+def todo():
+    return html.section(helloWorld(),
+                        helloWorld2(),
+                        PrintButton("SOU LABEL DO BOTAO", "SOU BOTAO TBM"))
+
+app = FastAPI()
+
+configure(app, todo)
