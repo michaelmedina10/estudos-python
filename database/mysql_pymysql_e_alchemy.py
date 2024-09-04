2ee5805f55159685f3a7f99a867f8659a547b01e
Michael Medina <68739172+michaelmedina10@users.noreply.github.com>
2024-09-04 13:30:24 -0300

    Update mysql_pymysql_e_sqlalchemy.py

--- database/mysql_pymysql_e_alchemy.py
+++ database/mysql_pymysql_e_alchemy.py
@@ -1,13 +1,9 @@
 from dataclasses import dataclass
 
+import pandas as pd
 import pymysql
-
-# Logger desenvolvido a partir da lib logging do python
 from settings.logs import logger
 from sqlalchemy import create_engine
-
-import pandas as pd
-import pandas as pd
 
 
 @dataclass
@@ -40,6 +36,15 @@ class MySqlConnection:
             self.connection.close()
 
     def execute_fetch(self, sql) -> pd.DataFrame:
+        """
+        Execute 'select query'.
+
+        Args:
+            sql (str): Query SQL
+
+        Returns:
+            pd.DataFrame
+        """
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute(sql)
@@ -52,6 +57,16 @@ class MySqlConnection:
             return pd.DataFrame()
 
     def execute_dml(self, sql: str, data: dict | list = None) -> bool:
+        """
+        Execute any DML instruction: 'insert, update, delete, truncate, etc'.
+
+        Args:
+            sql (str): Query SQL
+            data (dict| list): parameters of query
+
+        Returns:
+            bool: True to success False to some fail.
+        """
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute(sql, data)
@@ -64,13 +79,27 @@ class MySqlConnection:
 
     def insert_dataframe_in_mysql(
         self, df: pd.DataFrame, table_name: str, if_exists="append"
-    ):
+    ) -> bool:
+        """
+        Insert a dataframe using pandas.to_sql(). To use it is necessary install lib SQLAlchemy.
+
+        Args:
+            df (pd.DataFrame): Dataframe with data to be inserted
+            table_name (str): Name of table in database, only table name, no database name with table name.
+
+        Returns:
+            bool: True to success False to some fail.
+        """
         try:
             engine = create_engine(
                 f"mysql+pymysql://{self.user}:{self.pmysql}@{self.host}:{self.port}/{self.database}?charset=utf8"
             )
-            df.to_sql(table_name, engine, if_exists=if_exists, index=False)
-            return True
+            rows_affected = df.to_sql(
+                table_name, engine, if_exists=if_exists, index=False
+            )
+            if rows_affected is not None and rows_affected > 0:
+                return True
+            return False
         except Exception as e:
             logger.critical(f"Insert Error: {e}")
             return False
